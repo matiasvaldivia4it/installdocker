d49914d9f8ac0620ede5ea8babbc8890effdc660
Matias Valdivia <43653355+matiasvaldivia4it@users.noreply.github.com>
2025-08-01 19:26:08 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,7 +10,12 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \ 
-sudo docker run hello-world
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-worldsudo apt-get update
+
+sudo apt-get install docker-compose-plugin
+
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
 
